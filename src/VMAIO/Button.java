db0c3c20c3a7860c198dbee41f46a4fba54a7407
Hisarcode <hisarcode@gmail.com>
2019-06-21 16:13:53 +0700

    add button by gusti

--- src/VMAIO/Button.java
+++ src/VMAIO/Button.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package VMAIO;
-
+import java.util.Scanner;
 /**
  *
  * @author User
  */
 public class Button {
-    
+     public static void main(String[]args) {
+        Scanner input = new Scanner(System.in);
+        int[][] btn = {
+                {5, 5, 5, 5, 5},
+                {5, 5, 5, 5, 5}
+        };
+        int choose = input.nextInt();
+        for (int b = 0; b < 2; b++) {
+            for (int k = 0; k < 5; k++) {
+                if ((b == 0) && (choose < 6) && (choose!=0)) {
+                    if (choose - 1 == k) {
+                        btn[b][k] = btn[b][k] - 1;
+                    }
+                } else {
+                    if (choose - 6 == k) {
+                        btn[b][k] = btn[b][k] - 1;
+                    }
+                }
+            }
+        }
+    }
 }
